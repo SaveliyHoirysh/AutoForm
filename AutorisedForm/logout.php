705214898e5e6a3013a266a1a5b540727cce243b
SaveliyHoirysh <98319435+SaveliyHoirysh@users.noreply.github.com>
2024-01-13 18:06:43 +0500

    Add files via upload

---
+++ AutorisedForm/logout.php
@@ -0,0 +1,3 @@
+<?php
+setcookie("login", $login, -1, "/");
+setcookie("pass", $password, -1, "/");
---
+++ AutorisedForm/usersDB.php
@@ -0,0 +1,5 @@
+<?php
+$arrayUsers = [
+    ["login"=> "admin", "pass"=> "qwerty"],
+    ["login"=> "moderator", "pass"=> "password"],
+    ["login"=> "user", "pass"=> "123"] ];
